fef2079b2ffc3d992480bfd84014607fd1c96256
Rhonal Chirinos <122809074+rhonalindiefolks@users.noreply.github.com>
2023-12-30 01:29:47 -0300

    add database soport

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const pg = require('pg');
+
+const { Client } = pg;
+
+const config = {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'devops',
+    user: 'devops',
+    password: 'password'
+}
+const db = new Client(config)
+
 
 const init = async () => {
+    await db.connect(); 
 
-    const server = Hapi.server({
-        port: 3000,
-        host: '0.0.0.0'
-    });
+    const server = Hapi.server(
+        {
+            port: 3000,
+            host: '0.0.0.0'
+        }
+    ); 
 
     server.route({
         method: 'GET',
         path: '/',
-        handler: (request, h) => {
-
-            return 'Hello World!';
+        handler: async (request, h) => { 
+            const res = await db.query('SELECT * FROM people');
+            return h.response(res.rows);
         }
-    });
+    }); 
 
     await server.start();
+
     console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
 
     console.log(err);
+    db.close();
     process.exit(1);
 });
 
 init();
+
+
+
